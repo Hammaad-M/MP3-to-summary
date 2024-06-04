e37db81216566885a19c3fba8b0041b101234922
Hammaad-M <hammaad.memon@gmail.com>
2024-06-03 21:59:15 -0700

    add summarization component

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from transformers import WhisperProcessor, TFWhisperForConditionalGeneration
+from transformers import WhisperProcessor, TFWhisperForConditionalGeneration # For transcription
+from transformers import pipeline  # For text summarization
+import logging
+logging.getLogger("transformers").setLevel(logging.ERROR) 
 import librosa
 import tensorflow as tf
 from tqdm import tqdm
@@ -8,8 +11,10 @@ processor = WhisperProcessor.from_pretrained("openai/whisper-small")
 model = TFWhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
 model.config.forced_decoder_ids = None
 
-# Load the local audio file
+# # Load the local audio file
 audio_path = "t.mp3"
+transcript_file = "transcription.txt" # output for transcription
+summary_file = "summary.txt" # output for summary
 audio, sr = librosa.load(audio_path, sr=None)
 
 # Resample the audio to 16000 Hz
@@ -22,7 +27,7 @@ chunk_duration = 28  # seconds
 chunk_size = chunk_duration * sr
 
 # Open the text file for writing
-with open("transcription.txt", "w") as f:
+with open(transcript_file, "w") as f:
     # Process the audio in chunks and transcribe
     with tqdm(total=(len(audio) // chunk_size ) + 1, desc="Processing chunks") as pbar:
         for i, start in enumerate(range(0, len(audio), chunk_size), 0):
@@ -46,4 +51,22 @@ with open("transcription.txt", "w") as f:
             # Update progress bar
             pbar.update(1)
 
-print("Full transcription saved to transcription.txt")
+print("Full transcription saved to", transcript_file, "\nRunning summarization...")
+
+
+# Generate summary
+
+summary = []  # List to store summarized chunks
+
+with open(transcript_file, 'r', encoding='utf-8') as input_file:
+    with open(summary_file, 'w', encoding='utf-8') as output_file:
+        text = input_file.read()  # Read input text
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")  # Initialize summarizer model
+        text = [text[i:i+1200] for i in range(0, len(text), 1200)]  # Break text into chunks of 1200 characters
+
+        for chunk in tqdm(text, desc="Summarizing transcript chunks", unit="chunks"):  # Iterate over text chunks
+            print(min(500, len(chunk)))
+            summary = summarizer(chunk, max_length=min(250, len(chunk)), min_length=10, do_sample=True)  # Summarize each chunk, max_length is for the summary output
+            output_file.write(summary[0]['summary_text'] + "\n") 
+
+print("\nSummary saved to", summary_file)
